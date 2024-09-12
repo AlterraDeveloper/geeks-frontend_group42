70d53b8cd6b643ad5a30280a28dfd50cc78f16bb
Alterra$Developer <42990792+AlterraDeveloper@users.noreply.github.com>
2024-09-12 21:19:42 +0600

    add lesson 1

---
+++ lesson_1/app.js
@@ -0,0 +1,51 @@
+document.write("<h1>Hello, Group 42 from app.js</h1>");
+
+// V8 - JavaScript engine
+
+/*
+variables and data types
+переменные и типы данных
+
+1. string
+2. number
+3. boolean
+4. undefined
+5. null
+6. object
+
+7. bigint
+8. symbol
+
+
+*/
+
+console.log("Hello group 42 from console");
+// alert("Hello group 42 from alert");
+
+
+// var let const
+
+let countryName = "Kyrgyz Republic";
+let countryCode2 = 'KG';
+let countryCode3 = "KGZ";
+let countryFlag = "🇰🇬";
+
+console.log("Country info:", countryName, countryCode2,
+    countryCode3, countryFlag);
+
+let createdYear = 1991;
+let independenceYears = 33;
+let population = 6_542_451;
+let area = 520_450_452.87;
+let govDebt = -500_000_000_000_000;
+
+let hasArmy = true;
+let isEuropeanUnion = false;
+let hasMarineBoard = false;
+let isAsia = true;
+
+// https://github.com/AlterraDeveloper
+
+
+
+
